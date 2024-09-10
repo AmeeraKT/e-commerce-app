3277f1079259218eadead6656556acdb2761cfa8
AmeeraKT <cylestyy@gmail.com>
2024-09-10 20:00:48 +0700

    Added data

--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def show_main(request):
+    context = {
+        'name' : 'Cookie',
+        'price': '5000',
+        'description': 'Homey chocolate chip cookies baked fresh everyday'
+    }
+
+    return render(request, "main.html", context)
