77974f846384688bc0d93a22ac2e5a6d9473aff6
AmeeraKT <cylestyy@gmail.com>
2024-09-10 19:51:14 +0700

    Made models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    description = models.TextField()
+
+    #@property
+    #def is_mood_strong(self):
+    #    return self.mood_intensity > 5
