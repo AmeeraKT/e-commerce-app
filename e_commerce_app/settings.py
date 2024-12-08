1da6a49e981b3593ae0bf0e924fbe187feec398b
AmeeraKT <cylestyy@gmail.com>
2024-12-08 13:03:23 +0700

    Modified for assignment 9

--- e_commerce_app/settings.py
+++ e_commerce_app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-j3yqhx+&4dja3wdxqaf31&a*jf20vn(sznltd^!7qr_w_n=a1y
 PRODUCTION = os.getenv("PRODUCTION", False)
 DEBUG = not PRODUCTION
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "ameera-khaira-ecommerceapplication.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "ameera-khaira-ecommerceapplication.pbp.cs.ui.ac.id", "10.0.2.2"]
 
 
 # Application definition
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'authentication',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'e_commerce_app.urls'
@@ -132,3 +134,10 @@ else:
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://pbp.cs.ui.ac.id/web/project/ameera.khaira/ecommerceapplication", "https://pbp.cs.ui.ac.id/web/project/ameera.khaira/ecommerceapplication", "http://ameera-khaira-ecommerceapplication.pbp.cs.ui.ac.id", "https://ameera-khaira-ecommerceapplication.pbp.cs.ui.ac.id"]
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
